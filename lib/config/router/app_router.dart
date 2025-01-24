384817dba5ab65a1b1692e71663c4467e40198d6
FranCalveyra <franciscocalveyra24@gmail.com>
2025-01-24 15:59:51 -0300

    Minor refactors

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,8 +1,6 @@
 import 'package:cinemapedia/presentation/screens/home_screen.dart';
 import 'package:cinemapedia/presentation/screens/movies/movie_screen.dart';
-import 'package:cinemapedia/presentation/views/home_views/categories_view.dart';
 import 'package:go_router/go_router.dart';
-import 'package:cinemapedia/presentation/views/views.dart';
 
 final homeRoute = GoRoute(
     path: "/home/:page",
